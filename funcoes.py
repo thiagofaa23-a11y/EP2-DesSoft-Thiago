33005240d8594f8e310792e43c6615c98c75fd3a
thiagofaa23 <thiago.araujo.ismart@gmail.com>
2026-04-27 17:35:03 -0300

    guardar_dado

--- funcoes.py
+++ funcoes.py
@@ -9,3 +9,11 @@ def rolar_dados(numero_de_dados):
         i +=1
 
     return lista
+
+def guardar_dado(dados_rolados,dados_guardados,dado_selecionado):
+    
+    dados_guardados.append(dados_rolados[dado_selecionado])
+    del dados_rolados[dado_selecionado]
+    saida = [dados_rolados, dados_guardados]
+
+    return saida
